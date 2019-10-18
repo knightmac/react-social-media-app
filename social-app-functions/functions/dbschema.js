29a3a859aaabb2188c4db42465e10a2c80c6151c
Ofir Knimach <o.knimach@hotmail.com>
2019-10-18 12:33:51 +1000

    Stringified the creation date. Usage of express. creation of dbschema.js.

---
+++ social-app-functions/functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: "user",
+      body: "this is the scream body",
+      ceratedAt: "2019-10-18T02:21:18.094Z",
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+};
--- social-app-functions/functions/index.js
+++ social-app-functions/functions/index.js
@@ -2,24 +2,49 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
 admin.initializeApp();
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello world");
-});
 
-exports.getScreams = functions.https.onRequest((req, res) => {
+const express = require("express");
+const app = express();
+
+app.get("/screams", (req, res) => {
   admin
     .firestore()
     .collection("screens")
+    .orderBy("createdAt", "desc")
     .get()
     .then(data => {
       var screams = [];
       data.forEach(doc => {
-        screams.push(doc.data());
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt
+        });
       });
       return res.json(screams);
     })
     .catch(err => console.log(err));
 });
+
+app.post("/scream", (req, res) => {
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: new Date().toISOString()
+  };
+
+  admin
+    .firestore()
+    .collection("screens")
+    .add(newScream)
+    .then(doc => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch(err => {
+      res.status(500).json({ error: "something went wrong" });
+      console.error(err);
+    });
+});
+
+exports.api = functions.https.onRequest(app);
